ab2b0a529f776305b96e1f2ae7b082657ef544da
MackRoe <elaine.music@students.makeschool.com>
2021-04-29 19:50:20 -0400

    completed resolvers. server loads

--- server.js
+++ server.js
@@ -18,20 +18,46 @@ const typeDefs = gql`
 
 	type Subscription {
 		newPost: Post!
-	}
+	}`
 
-    const data = [
-    	{ message: 'hello world', date: new Date() }
-    ]
+const data = [
+	{ message: 'hello world', date: new Date() }
+]
 
-    const resolvers = {
+const resolvers = {
 	Query: {
 		// Query types
+        posts: () => {
+			return data
+        }
 	},
+    // Query: {
+    //     user(parent, args, context, info) {
+	// 		return users.find(user => user.id === args.id);
+    //     }
+    // },
 	Mutation: {
 		// Mutation types
+        addPost: (parent, args, context, info) => {
+			const post = { message, date: new Date() }
+			data.push(post)
+			pubsub.publish('NEW_POST', { newPost: post }) // Publish!
+			return post
+		}
 	},
 	Subscription: {
 		// Subscription types
-	}
-}
+        newPost: {
+			subscribe: () => pubsub.asyncIterator('NEW_POST')
+    		}
+    	}
+    }
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers
+});
+
+server.listen().then(({ url }) => {
+	console.log(`🚀 Server ready at ${url}`);
+});
