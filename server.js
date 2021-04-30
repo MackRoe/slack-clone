39cae443cef4dab48b1cb58c3d7268a9f698f0b0
MackRoe <elaine.music@students.makeschool.com>
2021-04-29 20:05:44 -0400

    debug resolvers

--- server.js
+++ server.js
@@ -31,14 +31,9 @@ const resolvers = {
 			return data
         }
 	},
-    // Query: {
-    //     user(parent, args, context, info) {
-	// 		return users.find(user => user.id === args.id);
-    //     }
-    // },
 	Mutation: {
 		// Mutation types
-        addPost: (parent, args, context, info) => {
+        addPost: (_, { message }) => {
 			const post = { message, date: new Date() }
 			data.push(post)
 			pubsub.publish('NEW_POST', { newPost: post }) // Publish!
